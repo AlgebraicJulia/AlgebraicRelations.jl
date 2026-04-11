535429e3a859f878f79d7d4f9e9720af3e0137a2
Matt Cuffaro <cuffaro.matt@gmail.com>
2026-04-11 02:09:44 -0600

    WIP: CSV ingestion

--- src/fabric/Fabric.jl
+++ src/fabric/Fabric.jl
@@ -209,6 +209,38 @@ function add_fk!(fabric::DataFabric, src::Int, tgt::Int, elabel::Pair{Symbol, Sy
 end
 export add_fk!
 
+using CSV
+
+function get_fk_type(fabric::DataFabric, col::Symbol)
+    col = incident(fabric.catalog, col, :cname)
+    from, = incident(fabric.catalog, col, :from)
+    only(subpart(fabric.catalog, from, [:to, :type]))
+end
+
+function is_fk_column(fabric::DataFabric, col::Symbol)
+    subpart(fabric.catalog, incident(fabric, :cname, col), :from) == 0
+end
+
+function ingest_csv!(fabric, table_name::Symbol, path::String)
+    df = CSV.read(path, DataFrame)
+    for row in eachrow(df)
+        kwargs = Dict{Symbol,Any}()
+        for col in names(df)
+            val = row[col]
+            # check if this column is an FK in the catalog
+            # TODO some redundancy
+            if is_fk_column(fabric, Symbol(col))
+                target_type = get_fk_type(fabric, Symbol(col))
+                kwargs[Symbol(col)] = FK{target_type}(val)
+            else
+                kwargs[Symbol(col)] = val isa String ? Symbol(val) : val
+            end
+        end
+        @info kwargs
+        add_part!(fabric, table_name; kwargs...)
+    end
+end
+export ingest_csv!
 
 # Executing commands on data fabric
 
